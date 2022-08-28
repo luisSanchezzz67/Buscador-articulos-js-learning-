24f76c0023e16dc0c936811ff9c60824f5833ce2
luis cruz <luiscrruzsanch3837@gmail.com>
2022-08-27 21:54:02 -0500

    Agregando valores a las opciones y agregando llenando el objecto de datos

---
+++ js/app.js
@@ -0,0 +1,102 @@
+//Variables
+const marca = document.querySelector("#marca");
+const year = document.querySelector("#year");
+const minimo = document.querySelector("#minimo");
+const maximo = document.querySelector("#maximo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+
+//Contenedor para los resultados
+const resultado = document.querySelector("#resultado");
+
+
+const max = new Date().getFullYear();
+const min = max - 10;
+
+//Generar un bojeto con la busqueda
+
+const datosBusqueda = {
+    marca : '',
+    year : '',
+    minimo : '',
+    maximo : '',
+    puertas : '',
+    transmision : '',
+    color : '',
+
+}
+
+
+
+
+//Eventos
+document.addEventListener("DOMContentLoaded", () => {
+    mostrarautos(); //Muestra los automoviles al cargar
+
+    //llena las opciones de años
+    llenarSelect();
+})
+
+
+//Event listener para los select de busqueda
+marca.addEventListener("change", (e) => {
+    datosBusqueda.marca = e.target.value;
+});
+
+year.addEventListener("change", (e) => {
+    datosBusqueda.year = e.target.value;
+    
+});
+
+minimo.addEventListener("change", (e) => {
+    datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener("change", (e) => {
+    datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener("change", (e) => {
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener("change", (e) => {
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener("change", (e) => {
+    datosBusqueda.color = e.target.value;
+    console.log(datosBusqueda);
+
+});
+
+
+//Funciones
+
+function mostrarautos() {
+    autos.forEach( auto => {
+
+        const {marca, modelo, year, puertas, transmision, precio, color} = auto;
+        const autoHTML = document.createElement('p');
+
+        autoHTML.textContent = `
+            ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision} - Precio: ${precio} - Color: ${color}
+        
+        `;
+
+        //insertar en el HTML
+        resultado.appendChild(autoHTML)
+    })
+}
+
+
+//Genera los años del select
+function llenarSelect() {
+    for (let i = max; i >= min; i--){
+        const opcion = document.createElement("option");
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion); //Agrega las opciones del año al select
+    }
+}
